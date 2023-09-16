815ce2bd99b4b0b975fe5907b9824ee5ef65421d
jeevanng <jjjeeevvvaaannn@gmail.com>
2023-09-16 11:38:15 +1000

    feat: pokemon API data being fetched

--- pokemonclass/src/PokemonDisplay.jsx
+++ pokemonclass/src/PokemonDisplay.jsx
@@ -14,11 +14,58 @@ export default class PokemonDisplay extends React.Component{
 		}
 	}
 
+    // E.g. facebook messenger, user is online
+    async componentDidMount(){
+        console.log("This message will appear once.");
+
+        // Grab a Pokemon and store its name 
+		
+		// Generate a random number or use a user-provided number
+		function getRandomPokemonId(){
+			return Math.floor(Math.random() * 1010) + 1
+		}
+
+        // Retrieve the API data
+		let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()).catch(error => {
+			throw new Error("API failure.");
+		});
+        console.log("Fetch has finished.");
+
+		if (response.status === 404){
+			throw new Error("API did not have data for the requested ID.");
+		}
+
+		// Convert the response into usable JSON 
+		let data = await response.json().catch(error => {
+			throw new Error("API did not return valid JSON.");
+		}); 
+
+        // Save pokemon name to component state
+        this.setState({pokemonName: data.name});
+    }
+
+    // E.g. facebook messenger, user is offline
+    componentWillUnmount(){
+        console.log("Component is being removed from the page.");
+    }
+
+    // Render is how we return JSX up to the React app
+    // so that something is displayed to the browser 
+
 	render(){
-		return(
-			<div>
-				<h1>Some content here</h1>
-			</div>
-		)
+
+		if (this.state.pokemonName) {
+			return (
+				<div>
+					<h1>{this.state.pokemonName}</h1>
+				</div>
+			)
+		} else {
+			return(
+				<div>
+					<h1>Loading...</h1>
+				</div>
+			)
+		}
 	}
 }
